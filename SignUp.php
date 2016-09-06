7abea901e8e5b1f0bb24466f41dfa056bfa30a24
Caesario Prima Bahari <crio.0592@gmail.com>
2016-09-06 15:21:53 +0700

    Isinya gua ilangin
    Gajlas pake gituan

--- SignUp.php
+++ SignUp.php
@@ -9,22 +9,7 @@
 				
 				<h2>Sign Up</h2><hr/>
 
-				<?php 
-				echo form_open('CSignUp/SignUp', 'role="form"');
-				?>
-				<label>Username 	:</label>
-					<input id="name" name="username" placeholder="Username" type="text">
-						<br><br>
-				<label>Password 	:</label>
-					<input id="password" name="password" placeholder="Password" type="password">
-						<br><br>
-				<label>Status 		:</label>
-					<input name="status" placeholder="Status" type="text">
-						<br><br>
-				<input type="submit" name="daftar" id="submit" value="Sign Up">
-				<?php
-				echo form_close();
-				?>
+				
 		</div>
 	</div>
 
